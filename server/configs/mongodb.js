86bdc1ffb65c4a73d16c183599958c4179d0b17d
Mohd Shan <mohdshande@gmail.com>
2025-12-30 20:19:09 +0100

    corrected error in MongoDb var names

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,7 +2,8 @@ import mongoose, { mongo } from "mongoose";
 //connect to the MongoDb database
 const connectDB = async ()=>{
     mongoose.connection.on('connected', ()=> console.log('Database connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/ShanAcademy`)
+    await mongoose.connect(`${process.env.MONGO_URI}/ShanAcademy`)
+
 
 }
 export default connectDB
